aa7abbe75b502debcd7185eb47db7957ca80b064
romichoirudin33 <romichoirudin33@gmail.com>
2019-09-10 14:43:47 +0700

    fix axios

--- app/Http/Controllers/ValuePersonelController.php
+++ app/Http/Controllers/ValuePersonelController.php
@@ -2,9 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Kategori;
+use App\Models\Personel;
 use Illuminate\Http\Request;
 
 class ValuePersonelController extends Controller
 {
-    //
+    public function create(Request $request)
+    {
+        $kategori = $request->get('kategori');
+        $id = $request->get('id');
+        if (empty($kategori) || empty($id)){
+            abort(500);
+        }
+
+        $kategori = Kategori::where('id', $kategori)->firstOrFail();
+        $personel = Personel::where('uuid', $id)->firstOrFail();
+
+        if ($kategori->is_many == '1'){
+
+        }
+    }
 }
